263afa88249d8535f22d3dda261b233334143a8e
hamelin123 <107036536+hamelin123@users.noreply.github.com>
2026-09-15 12:50:35 +0000

    shield 0.6.2

--- Formula/shield.rb
+++ Formula/shield.rb
@@ -1,28 +1,28 @@
 class Shield < Formula
   desc "Code security scanner: SAST, secrets, dependencies, containers and IaC"
   homepage "https://zennoxa.com"
-  version "0.6.1"
+  version "0.6.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/Zennoxa/shield/releases/download/v0.6.1/shield-darwin-amd64"
-      sha256 "f2572c771f6e93d0361148f8aaabbc19383220741b30605325cdfc8dc473fee2"
+      url "https://github.com/Zennoxa/shield/releases/download/v0.6.2/shield-darwin-amd64"
+      sha256 "680fa45960a6f214ddb2bee829b204a849479794715fde0687f114fa26b39a73"
     end
     on_arm do
-      url "https://github.com/Zennoxa/shield/releases/download/v0.6.1/shield-darwin-arm64"
-      sha256 "35eaf465ff99fa8367046d9b976e39de5ba4c7e7dce984546cc49c7e2644e0a1"
+      url "https://github.com/Zennoxa/shield/releases/download/v0.6.2/shield-darwin-arm64"
+      sha256 "d68616230176e8d24c1cbaf80e92ff265f8922d21dbc0a86277dfdc3dc1395a1"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Zennoxa/shield/releases/download/v0.6.1/shield-linux-amd64"
-      sha256 "8caf7d573fe972a9d66a60c0123beb9770daa51bf941b928b27b6090635a7f75"
+      url "https://github.com/Zennoxa/shield/releases/download/v0.6.2/shield-linux-amd64"
+      sha256 "927e01acc4bd297c7eb18b9d5c529a37de0849dc6afc05ce05999d80b416bd6a"
     end
     on_arm do
-      url "https://github.com/Zennoxa/shield/releases/download/v0.6.1/shield-linux-arm64"
-      sha256 "385eb9c2f54200489f79815adf0bf188c33880bf879fa5727e2c37bddcf9de63"
+      url "https://github.com/Zennoxa/shield/releases/download/v0.6.2/shield-linux-arm64"
+      sha256 "72d771f6bfe9a1e82f198b32276c0c79bd632df31bda9a002545f3ac30eb904a"
     end
   end
 
@@ -31,6 +31,6 @@ class Shield < Formula
   end
 
   test do
-    assert_match "0.6.0", shell_output("#{bin}/shield version")
+    assert_match "0.6.2", shell_output("#{bin}/shield version")
   end
 end
